eb60c27952696effadce520800c0a31649f95de3
Luca Pagnossin <lucapagnossinhemmingsen@gmail.com>
2022-10-01 20:27:25 -0300

    Portfolio de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    FooterComponent,
+    PresentationComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
